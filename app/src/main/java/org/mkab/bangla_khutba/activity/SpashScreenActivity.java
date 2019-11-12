361fb2a221da2eb0a6c1a61d881455ac842d74fe
supan09 <supan@aci-bd.com>
2019-11-12 17:52:09 +0600

    Update Splash screen

--- app/src/main/java/org/mkab/bangla_khutba/activity/SpashScreenActivity.java
+++ app/src/main/java/org/mkab/bangla_khutba/activity/SpashScreenActivity.java
@@ -16,20 +16,12 @@ import org.mkab.bangla_khutba.R;
 
 public class SpashScreenActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 1000;
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        SharedPreferences prefs = getSharedPreferences("PHONE_NUMBER", MODE_PRIVATE);
-
-        if (prefs.getBoolean("signIn", false)) {
-            //findViewById(R.id.btn_advance).setVisibility(View.GONE);
-            startActivity(new Intent(this, KhutbaListActivity.class));
-            finish();
-        }
 
         new Handler().postDelayed(new Runnable() {
 
@@ -58,6 +50,7 @@ public class SpashScreenActivity extends AppCompatActivity {
                     Toast.makeText(SpashScreenActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
                 }
             }
+
             startActivity(new Intent(this, KhutbaListActivity.class));
             finish();
         }
